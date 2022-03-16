320d911b5ffef327e9ac0379a12bf5183945d278
Ievule666 <101431267+Ievule666@users.noreply.github.com>
2022-03-16 19:21:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firstlabsecondtry/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.firstlabsecondtry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
